ee0f6e0bde7bd6ee9e2a18353377552446b334c3
Koya Gabriel <koyexes@stakeholder.local>
2017-09-20 11:05:59 +0100

    server side validation of data

--- client/components/SignUpForm.js
+++ client/components/SignUpForm.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import css from './css/SignUpForm.css';
+import classnames from 'classnames';
 
 class SignUpForm extends React.Component {
 
@@ -12,7 +13,8 @@ class SignUpForm extends React.Component {
       username: '',
       email: '',
       password: '',
-      passwordConfirmation: ''
+      passwordConfirmation: '',
+      errors: {}
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -24,29 +26,40 @@ class SignUpForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    this.props.userSignupRequest(this.state);
+    this.setState( {errors: {} });
+    this.props.userSignupRequest(this.state).then(
+      () => {},
+      ({ response }) => {
+        this.setState({errors: response.data});
+      }
+    );
   }
 
   render() {
+    const { errors } = this.state;
     return(
       <div className="d-flex justify-content-center">
         <form className={"w-50 " + css.signUpForm } onSubmit={this.onSubmit}>
           <h1 className={"d-flex justify-content-center " + css.header}>Join our community!</h1>
-          <div className="form-group">
+          <div className={classnames("form-group", { "has-danger": errors.username })}>
             <label className="font-weight-bold" >Username</label>
             <input type="text" className="form-control" name="username" placeholder="Username" value={this.state.username} onChange={this.onChange} />
+            { errors.username && <span>{errors.username}</span> }
           </div>
-          <div className="form-group">
+          <div className={classnames("form-group", { hasDanger: errors.email })}>
             <label className="font-weight-bold" >Email</label>
             <input type="email" className="form-control" name="email" placeholder="Email" value={this.state.email} onChange={this.onChange} />
+            { errors.email && <span>{errors.email}</span> }
           </div>
-          <div className="form-group">
+          <div className={classnames("form-group", { "has-danger": errors.password })}>
             <label className="font-weight-bold" >Password</label>
             <input type="password" className="form-control" name="password" placeholder="Password" value={this.state.password} onChange={this.onChange} />
+            { errors.password && <span>{errors.password}</span> }
           </div>
-          <div className="form-group">
+          <div className={classnames("form-group", { "has-danger": errors.passwordConfirmation })}>
             <label className="font-weight-bold" >Password Confirmation</label>
             <input type="password" className="form-control" name="passwordConfirmation" placeholder="Password" value={this.state.passwordConfirmation} onChange={this.onChange} />
+            { errors.passwordConfirmation && <span>{errors.passwordConfirmation}</span> }
           </div>
           <div className="form-group d-flex justify-content-center">
             <button type="submit" className="btn btn-primary w-25 " >Sign up</button>
